0426f0478336d38cf8dddf7daaeb314289cd6c38
sbell9 <=>
2020-07-08 14:18:55 -0500

    patch and delete recipes

--- recipeapi/recipes/routes.py
+++ recipeapi/recipes/routes.py
@@ -3,18 +3,42 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 from recipeapi.recipes.models import Recipe
 from recipeapi.recipes.schema_validators import validate_recipe_post
 from recipeapi.marshmallow_utils import convert_errors_to_sentence
-from recipeapi.recipes.services import save_new_recipe
+from recipeapi.recipes.services import save_new_recipe, get_recipe, update_recipe, delete_recipe
 
 recipes = Blueprint('recipes', __name__)
 
 
 @recipes.route('/recipes', methods=['POST'])
 @jwt_required
-def create_recipe():
+def create():
     body = request.json
     errors = validate_recipe_post(body)
     if errors:
         abort(400, description=convert_errors_to_sentence(errors))
     user = get_jwt_identity()
     save_new_recipe(body, user.get('user_id'))
+    return Response(None, 201)
+
+@recipes.route('/recipes/<recipe_id>', methods=['PATCH'])
+@jwt_required
+def patch(recipe_id):
+    body = request.json
+    # make sure someone else isn't updating record
+    logged_in_user = get_jwt_identity()
+    existing_recipe = get_recipe(recipe_id)
+    if logged_in_user.get('user_id') != str(existing_recipe.user_id):
+        abort(401, description='You do not have permission to do that.')
+    update_recipe(existing_recipe, body)
+    return Response(None, 200)
+
+@recipes.route('/recipes/<recipe_id>', methods=['DELETE'])
+@jwt_required
+def delete(recipe_id):
+    body = request.json
+    # make sure someone else isn't updating record
+    logged_in_user = get_jwt_identity()
+    existing_recipe = get_recipe(recipe_id)
+    if logged_in_user.get('user_id') != str(existing_recipe.user_id):
+        abort(401, description='You do not have permission to do that.')
+    delete_recipe(recipe_id)
     return Response(None, 200)
--- recipeapi/recipes/services.py
+++ recipeapi/recipes/services.py
@@ -12,3 +12,22 @@ def save_new_recipe(body, uploaded_by):
                     user_id=uploaded_by)
     db.session.add(recipe)
     db.session.commit()
+
+def get_recipe(recipe_id):
+    return Recipe.query.filter_by(recipe_id=recipe_id).first()
+
+
+def update_recipe(existing_recipe, body):
+    existing_recipe.name = body.get('name')
+    existing_recipe.instructions = body.get('instructions')
+    existing_recipe.ingredients = body.get('ingredients')
+    existing_recipe.utilities = body.get('utilities')
+    existing_recipe.est_completion_time_hrs = body.get('est_completion_time_hrs')
+    existing_recipe.min_serving_size = body.get('min_serving_size')
+    existing_recipe.max_serving_size = body.get('max_serving_size')
+    db.session.commit()
+
+# https://stackoverflow.com/a/27159298/7858114
+def delete_recipe(recipe_id):
+    Recipe.query.filter_by(recipe_id=recipe_id).delete()
+    db.session.commit()
